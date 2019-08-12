1ee06ed94d0dc32d590095581c7cc27d8c5d2afb
eothein <jens.buysse@gmail.com>
2019-08-12 10:54:11 +0200

    [feat] Add a toast when button clicked

--- app/src/main/java/be/equality/diceroller/MainActivity.kt
+++ app/src/main/java/be/equality/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package be.equality.diceroller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +15,10 @@ class MainActivity : AppCompatActivity() {
         roll_button.text = "Let's Roll"
         roll_button.setOnClickListener {
             Log.i("MainAxtivity", "Rolled")
+            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
         }
+
+
+
     }
 }
