584483ab7b28f1a5a68bd14c7aaf9c12705dd791
eothein <jens.buysse@gmail.com>
2019-08-12 11:09:05 +0200

    [feat] Add imageView and change contents based on roll

--- app/src/main/java/be/equality/diceroller/MainActivity.kt
+++ app/src/main/java/be/equality/diceroller/MainActivity.kt
@@ -26,7 +26,14 @@ class MainActivity : AppCompatActivity() {
      * Generates a random integer and assigns its value to the TextView.
      */
     private fun rollDice() {
-        val randomInt = Random().nextInt(6) + 1
-        result_text.text = randomInt.toString()
+        val drawableResource = when (Random().nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        dice_image.setImageResource(drawableResource)
     }
 }
