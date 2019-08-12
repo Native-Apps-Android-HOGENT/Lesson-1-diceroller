40a7b45be31fcbb7552afbd49360f91e0b5821c9
eothein <jens.buysse@gmail.com>
2019-08-12 11:05:27 +0200

    [feat] Add roll dice and change textview

--- app/src/main/java/be/equality/diceroller/MainActivity.kt
+++ app/src/main/java/be/equality/diceroller/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,11 +15,18 @@ class MainActivity : AppCompatActivity() {
 
         roll_button.text = "Let's Roll"
         roll_button.setOnClickListener {
-            Log.i("MainAxtivity", "Rolled")
-            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
 
 
 
+    }
+
+    /**
+     * Generates a random integer and assigns its value to the TextView.
+     */
+    private fun rollDice() {
+        val randomInt = Random().nextInt(6) + 1
+        result_text.text = randomInt.toString()
     }
 }
