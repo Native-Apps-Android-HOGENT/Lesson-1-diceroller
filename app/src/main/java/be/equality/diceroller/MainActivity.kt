911ce8d2b12a7146d2639482e2fcd51ee4feacd1
eothein <jens.buysse@gmail.com>
2019-08-12 10:51:35 +0200

    [feat] Find the Button in the Activity using extentions

--- app/src/main/java/be/equality/diceroller/MainActivity.kt
+++ app/src/main/java/be/equality/diceroller/MainActivity.kt
@@ -2,11 +2,18 @@ package be.equality.diceroller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        roll_button.text = "Let's Roll"
+        roll_button.setOnClickListener {
+            Log.i("MainAxtivity", "Rolled")
+        }
     }
 }
