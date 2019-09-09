9b5af78c0ddeba3c2a3b4f239e3b8f3f7e1ec96e
Harm De Weirdt <harm.deweirdt@hogent.be>
2019-09-09 15:38:48 +0200

    chore: rewrite to  more idiomatic Kotlin

--- app/src/test/java/be/equality/diceroller/RandomRollerTest.kt
+++ app/src/test/java/be/equality/diceroller/RandomRollerTest.kt
@@ -10,13 +10,15 @@ class RandomRollerTest {
      * Tests whether the [RandomRoller] generates numbers between the given integers.
      */
     @Test
-    fun testRoller(){
-        val rRoller = RandomRoller(1,6)
+    fun testRoller() {
+        val rRoller = RandomRoller(1, 6)
 
-        for (x in 1 .. 10){
+        repeat(100) {
             val number = rRoller.roll()
-            Assert.assertTrue("Number is too high", number <= 6)
-            Assert.assertTrue("Number is too low", number >= 1)
+            Assert.assertTrue(
+                "RandomRoller generated a number outside of the specified range.",
+                (1..6).contains(number)
+            )
         }
     }
 }
